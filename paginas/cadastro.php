0b756c2e6537713dac1760f86b984d11ecbd95ba
VitorGiam <vitor.giam@hotmail.com>
2016-12-04 10:53:23 -0200

    parte 3

--- paginas/cadastro.php
+++ paginas/cadastro.php
@@ -1,45 +1,68 @@
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <title>Cadastro</title>
-    <link rel="icon" href="imagens/favicon.ico">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Blank Template for Bootstrap</title>
+
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <link href="style.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../incremento/Style.css">
+
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
 </head>
-<body>
-<div class="container">
-    <form class="form-signin" method="POST" action="cadastro_acao.php">
-        <h2 class="form-signin-heading">Cadastro</h2>
-        Nome: <input type="text" name="txt_nome" id="inputnome" class="form-control" placeholder="Nome" required autofocus>
+<br>
+<header>
+    <div class="col-md-12 text-center">
+        <img src="../imagens/bigode.png" width="200" />
+    </div>
+</header>
+    <body class="body-background">
+        <div class="container background-configuration">
+            <form class="form-signin" method="POST" action="cadastro_acao.php" >
+                <h2 class="form-signin-heading">Cadastro</h2>
+                Nome:
 
+                <input type="text" name="txt_nome" id="inputnome" class="form-control box-formatation" placeholder="Nome" required autofocus>
+                <br>
+                Email:
 
-        <br>
-        Email:<input type="email" name="txt_email" id="inputEmail" class="form-control" placeholder="Email" required
-               autofocus>
-        <br>
-        Senha:<input type="password" name="txt_senha" id="inputsenha" class="form-control" placeholder="Senha" required
-               autofocus>
-        <br>Idade: <input type="number" step="1" name="txt_idade" id="inputidade" min="1" class="form-control" placeholder="Idade"
-               required autofocus>
-        <br>
-        Peso (Kg): <input type="number" step="0.1" name="txt_peso" id="inputPeso" min="1" class="form-control" placeholder="Peso"
-               required autofocus>
-        <br>
-        Altura(m):<input type="number" step="0.1" name="txt_altura" id="inputAltura" min="1" class="form-control"
-               placeholder="Altura" required autofocus>
-        <br>
-        <label class="radio-inline">
-            <input type="radio" name="txt_sexo" id="Homem" value="h" checked>Homem
-        </label>
-        <label class="radio-inline">
-            <input type="radio" name="txt_sexo" id="mulher" value="m" >Mulher
-        </label>
-        <br>
-        <br>
-        <button class="btn btn-lg btn-success btn-block" type="submit">Salvar</button>
-        <p class="text-center text-danger">
-    </form>
-</div>
-</body>
+                <input type="email" name="txt_email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+                <br>
+                Senha:
+
+                <input type="password" name="txt_senha" id="inputsenha" class="form-control" placeholder="Senha" required autofocus>
+                <br>Idade:
+                <input type="number" step="1" name="txt_idade" id="inputidade" min="1" class="form-control" placeholder="Idade" required autofocus>
+                <br>
+                Peso (Kg):
+
+                <input type="number" step="0.1" name="txt_peso" id="inputPeso" min="1" class="form-control" placeholder="Peso" required autofocus>
+                <br>
+                Altura(m):
+
+                <input type="number" step="0.1" name="txt_altura" id="inputAltura" min="1" class="form-control" placeholder="Altura" required autofocus>
+                <br>
+                <label class="radio-inline">
+                    <input type="radio" name="txt_sexo" id="Homem" value="h" checked>Homem
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="txt_sexo" id="mulher" value="m">Mulher
+                </label>
+                <br>
+                <br>
+                <button class="btn btn-lg btn-success btn-block" type="submit">Salvar</button>
+                <p class="text-center text-danger">
+            </form>
+        </div>
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+    </body>
 </html>
--- valida.php
+++ valida.php
@@ -5,17 +5,17 @@ include("config bd/conexao_bd.php");
 $email = @$_POST['txt_email'];
 $senha = @$_POST['txt_senha'];
 
-header("Location: paginas/perfil.php");
+// header("Location: paginas/perfil.php");
 
 function consulta_usuario_bd($senha, $email)
 {
     $conn = new conexao_bd();
-    $result1 = $conn->query("SELECT * FROM pessoa WHERE email = '$email' LIMIT 1");
+    $result1 = $conn->query("SELECT * FROM pessoa WHERE idCodigoPessoa = 1 ");
     if (!empty($result1)) {
         if ($fetch = $result1->fetch_assoc()) {
             foreach ($fetch as $field => $value) {
-                echo '<pre>';print_r($result->fetch_all());exit;
-                header("Location: paginas/perfil.php");
+                // echo '<pre>';print_r($result->fetch_all());exit;
+                //header("Location: paginas/perfil.php");
                 if ($field == 'senha') {
                     if ($value != $senha) {
                         header('Location: index.html');
@@ -28,10 +28,10 @@ function consulta_usuario_bd($senha, $email)
                         exit;
                     } else {
                         $_SESSION['email'] = $value;
-                        header("Location: paginas/perfil.php");exit;
+                        // header("Location: paginas/perfil.php");exit;
                     }
                 }
-                if ($field == 'id') {
+                if ($field == 'idCodigoPessoa') {
                     $_SESSION['id'] = $value;
                 }
                 if ($field == 'nome') {
@@ -40,8 +40,8 @@ function consulta_usuario_bd($senha, $email)
                 if ($field == 'sexo') {
                     $_SESSION['sexo'] = $value;
                 }
-                //header("Location: paginas/perfil.php");exit;
             } //foreach
+            header("Location: paginas/perfil.php");exit;
         } // fetch
     } else {
         header("Location: index.html");
@@ -49,4 +49,4 @@ function consulta_usuario_bd($senha, $email)
     }
 }
 
-//consulta_usuario_bd($senha, $email);
+consulta_usuario_bd($senha, $email);
